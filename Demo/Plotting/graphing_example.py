19e8a2b378f287a1a64e18cb732af11f70c56879
dustykeyboard1 <mikesyanks02@icloud.com>
2023-09-03 13:10:39 -0400

    Additions

---
+++ Demo/Plotting/graphing_example.py
@@ -0,0 +1,18 @@
+'''
+Author:
+Michael Scoleri
+Violet Shi 
+James Gallagher 
+
+Data: 09/03/2023
+
+Filename: graphing_example.py
+
+Function: 
+Demonstrates graphing capabilities of software and saves to a PDF. 
+Includes a coninuous heatmap,
+'''
+
+def main():
+    heatmap = heatmap()
+    
