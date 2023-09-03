6ec1f894ed2cbf351b034c9ea95d9a438b25c1f3
James Gallagher <gallagh.jamesp@gmail.com>
2023-09-03 19:16:48 -0400

    Interpolation intial test and python function

--- Demo/Test Folder/interpolation_test.py
+++ Demo/Test Folder/interpolation_test.py
@@ -1042,32 +1042,7 @@ def main():
         print("No differences! Hooray!")
 
     
-    '''
-    The interpolate function in matlab, which has the following structure:
-    interp1(x, v, xq, method), where... 
-        x is the x-values of the known points
-        v is the y-values of the known points
-        xq is all the points we want to know
-        method specifies how to fit the data (linear, cubic, nearest, etc.)
 
-    In order to translate this into Python, we use scipy's interp1d method.
-    This method takes the following parameters
-    interp1d(x, y, kind), where...
-        x is the x-values
-        y is the y-values
-        kind specifies how to fit the data (linear, cubic, nearest, etc.)
-    
-    This interp1 function returns a function. We have to pass the data we want 
-    to fit to this function. We can accomplish the matlab function 
-    with a two step python function:
-    
-    f = interp1d(x, y, kind)                    # This gives us an interpolation function
-    array = []
-    for query_point in xq:
-        array.append(f(query_point))            # This gives us a 1D array
-    
-    We can use scipy's interp1d function to accomplish the same thing as Matlab's interp1 function
-    '''
 
 
 if __name__=="__main__":
