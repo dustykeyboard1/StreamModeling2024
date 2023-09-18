9e1732c509de17a2a4e3ec02a226efb29e60e808
dustykeyboard1 <mikesyanks02@icloud.com>
2023-09-18 13:36:30 -0400

    First Test

--- Python/Tests/test_hflux_error.py
+++ Python/Tests/test_hflux_error.py
@@ -1,18 +1,22 @@
+"""
+Author: Michael Scoleri
+Date: 09-18-23
+File: test_hflux_errors.py
+Functionality: Test the performance of hflux_errors.py
+"""
+
 import sys
 import os
 import pytest
 import numpy as np
 
-
-root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+#Find the root directory dynmimically. https://stackoverflow.com/questions/73230007/how-can-i-set-a-root-directory-dynamically
+root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
 sys.path.append(root_dir)
-
-print(root_dir)
-
 from Python.src.Core.hflux_errors import handle_errors
 
-
 def test_valid_arguments():
+    """Test if `handle_errors` accepts 6 or 7 arguments without raising exceptions."""
     time_mod = np.array([[1], [2], [3]])
     dist_mod = np.array([[1], [2], [3]])
     temp_mod = np.array([[1, 2], [3, 4], [5, 6]])
@@ -20,18 +24,18 @@ def test_valid_arguments():
     time_temp = np.array([[1], [2], [3]])
     temp = np.array([[1, 2], [3, 4], [5, 6]])
 
-    # Test with 6 arguments
     handle_errors(time_mod, dist_mod, temp_mod, dist_temp, time_temp, temp)
 
-    # Test with 7 arguments
     handle_errors(time_mod, dist_mod, temp_mod, dist_temp, time_temp, temp, True)
 
 def test_invalid_number_of_arguments():
+    """Test if handle_errors will raise a ValueError exception when given incorrect number of arguments"""
     with pytest.raises(ValueError):
         handle_errors(1, 2, 3)
 
 def test_invalid_column_vector():
-    time_mod = np.array([1, 2, 3])  # Not a column vector
+    """Test if handle_errors will raise a TypeError exception when given the incorrect shape of Array"""
+    time_mod = np.array([1, 2, 3])
     dist_mod = np.array([[1], [2], [3]])
     temp_mod = np.array([[1, 2], [3, 4], [5, 6]])
     dist_temp = np.array([[1], [2], [3]])
--- Python/src/Core/hflux_errors.py
+++ Python/src/Core/hflux_errors.py
@@ -11,23 +11,34 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d
 
-
 def handle_errors(*args):
+    """
+    Checks for errors related to the heat flux calculations.
+        - Checks Value Errors for number of arguments.
+        - Checks Type Errors for type/shape of arguments
+    Params: *args - list of arguements to be assigned.
+    Returns: null
+    """
 
+    #Initialize variables. 
     unattend = False
     num_args = len(args)
 
+    #Check if the number of arguments is correct.
     if num_args not in [6, 7]:
         raise ValueError("Invalid number of arguments")
     
+    #Assign arguments to variables based on number of arguments.
     if num_args == 7:
         time_mod, dist_mod, temp_mod, dist_temp, time_temp, temp, unattend = args
     else:
         time_mod, dist_mod, temp_mod, dist_temp, time_temp, temp = args
 
+    #Check for Boolean, still to be figured out. 
     if unattend:
         print("Checking input arguments...")
 
+    #Type check variables to ensure they are column vectors. 
     if time_mod.ndim != 2 or time_mod.shape[1] != 1:
         raise TypeError("Time_m must be a column vector")
     if dist_mod.ndim != 2 or dist_mod.shape[1] != 1:
