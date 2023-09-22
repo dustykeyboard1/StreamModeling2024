cbfc882640a3deb3d137a702338121dedf46962f
dustykeyboard1 <mikesyanks02@icloud.com>
2023-09-22 13:53:42 -0400

    Changed to row vec

---
+++ Python/Scripts/script_to_run.py
@@ -0,0 +1,26 @@
+"""
+Author: Michael Scoleri
+Date: 09-22-23
+File: script_to_run.py
+Functionality: Coordinate the entire program
+"""
+
+import pandas as pd
+import numpy as np
+import sys
+import os
+root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+sys.path.append(root_dir)
+print(root_dir)
+
+from Python.src.Utilities.Input_reader import readFromFile
+from Python.src.Core.hflux_errors import handle_errors
+
+input_data = readFromFile('Python/Data/example_data.xlsx')
+
+print((input_data['time_mod'][0]))
+
+temp_mod = np.array([[1, 2, 7], [3, 4, 8], [5, 6, 9]])
+
+rel_err, me, mae, mse, rmse, nrmse = handle_errors(input_data['time_mod'], input_data['dist_mod'], temp_mod, input_data['temp_t0_data'][:, 0], 
+                                                   input_data['temp_x0_data'][:, 0], input_data['temp'])
