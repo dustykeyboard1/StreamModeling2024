e4bd24c53aa9762b5cef1059c94963582d00cf51
dustykeyboard1 <mikesyanks02@icloud.com>
2023-09-22 14:01:58 -0400

    Changed back to col vectors

--- Python/Tests/test_hflux_error.py
+++ Python/Tests/test_hflux_error.py
@@ -10,22 +10,23 @@ import os
 import pytest
 import numpy as np
 
-#Find the root directory dynamically. https://stackoverflow.com/questions/73230007/how-can-i-set-a-root-directory-dynamically
+#Find the root directory dynmimically. https://stackoverflow.com/questions/73230007/how-can-i-set-a-root-directory-dynamically
 root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
 sys.path.append(root_dir)
-
+print(root_dir)
 from Python.src.Core.hflux_errors import handle_errors
 
 def test_valid_arguments():
     """Test if `handle_errors` accepts 6 or 7 arguments without raising exceptions."""
-    time_mod = np.array([[1, 2, 3]])
-    dist_mod = np.array([[1, 2, 3]])
+    time_mod = np.array([[1], [2], [3]])
+    dist_mod = np.array([[1], [2], [3]])
     temp_mod = np.array([[1, 2], [3, 4], [5, 6]])
-    dist_temp = np.array([[1, 2, 3]])
-    time_temp = np.array([[1, 2, 3]])
+    dist_temp = np.array([[1], [2], [3]])
+    time_temp = np.array([[1], [2], [3]])
     temp = np.array([[1, 2], [3, 4], [5, 6]])
 
     handle_errors(time_mod, dist_mod, temp_mod, dist_temp, time_temp, temp)
+
     handle_errors(time_mod, dist_mod, temp_mod, dist_temp, time_temp, temp, True)
 
 def test_invalid_number_of_arguments():
@@ -33,13 +34,13 @@ def test_invalid_number_of_arguments():
     with pytest.raises(ValueError):
         handle_errors(1, 2, 3)
 
-def test_invalid_row_vector():
+def test_invalid_column_vector():
     """Test if handle_errors will raise a TypeError exception when given the incorrect shape of Array"""
-    time_mod = np.array([1, 2, 3])  # Not a row vector
-    dist_mod = np.array([[1, 2, 3]])
+    time_mod = np.array([1, 2, 3])
+    dist_mod = np.array([[1], [2], [3]])
     temp_mod = np.array([[1, 2], [3, 4], [5, 6]])
-    dist_temp = np.array([[1, 2, 3]])
-    time_temp = np.array([[1, 2, 3]])
+    dist_temp = np.array([[1], [2], [3]])
+    time_temp = np.array([[1], [2], [3]])
     temp = np.array([[1, 2], [3, 4], [5, 6]])
 
     with pytest.raises(TypeError):
@@ -47,17 +48,20 @@ def test_invalid_row_vector():
 
 def test_check_matrix_type():
     """Test if handle_errors will raise a TypeError exception when given non-matrix types for temp_mod and temp."""
-    time_mod = np.array([[1, 2, 3]])
-    dist_mod = np.array([[1, 2, 3]])
+    time_mod = np.array([[1], [2], [3]])
+    dist_mod = np.array([[1], [2], [3]])
     temp_mod = np.matrix([[1, 2], [3, 4], [5, 6]])  # Not an array
-    dist_temp = np.array([[1, 2, 3]])
-    time_temp = np.array([[1, 2, 3]])
-    temp = np.matrix([[1, 2], [3, 4], [5, 6]])  # Not an array
+    dist_temp = np.array([[1], [2], [3]])
+    time_temp = np.array([[1], [2], [3]])
+    temp = np.matrix([[1, 2], [3, 4], [5, 6]])  # Not a array
 
     with pytest.raises(TypeError):
         handle_errors(time_mod, dist_mod, temp_mod, dist_temp, time_temp, temp)
 
     temp_mod = np.array([[1, 2], [3, 4], [5, 6]])
-    temp = np.array([[1, 2], [3, 4], [5, 6]])
+    temp = np.array([[1, 2], [3, 4], [5, 6]])  
 
+    # Should not raise an exception
     handle_errors(time_mod, dist_mod, temp_mod, dist_temp, time_temp, temp)
+
+    
--- Python/src/Core/hflux_errors.py
+++ Python/src/Core/hflux_errors.py
@@ -24,34 +24,40 @@ def handle_errors(*args):
     Params: *args - list of arguements to be assigned.
     Returns: null
     """
+
+    #Initialize variables. 
     unattend = False
     num_args = len(args)
 
+    #Check if the number of arguments is correct.
     if num_args not in [6, 7]:
         raise ValueError("Invalid number of arguments")
     
+    #Assign arguments to variables based on number of arguments.
     if num_args == 7:
         time_mod, dist_mod, temp_mod, dist_temp, time_temp, temp, unattend = args
     else:
         time_mod, dist_mod, temp_mod, dist_temp, time_temp, temp = args
 
+    #Check for Boolean, still to be figured out. 
     if unattend:
         print("Checking input arguments...")
 
-    # Check for row vectors
-    if time_mod.ndim != 2 or time_mod.shape[0] != 1:
-        raise TypeError("Time_mod must be a row vector.")
-    if dist_mod.ndim != 2 or dist_mod.shape[0] != 1:
-        raise TypeError("Dist_mod must be a row vector.")
-    if time_temp.ndim != 2 or time_temp.shape[0] != 1:
-        raise TypeError("Time_temp must be a row vector.")
-    if dist_temp.ndim != 2 or dist_temp.shape[0] != 1:
-        raise TypeError("Dist_temp must be a row vector.")
+    #Type check variables to ensure they are column vectors. 
+    if time_mod.ndim != 2 or time_mod.shape[1] != 1:
+        raise TypeError("Time_mod must be a column vector.")
+    if dist_mod.ndim != 2 or dist_mod.shape[1] != 1:
+        raise TypeError("Dist_m must be a column vector.")
+    if time_temp.ndim != 2 or time_temp.shape[1] != 1:
+        raise TypeError("Time_temp must be a column vector.")
+    if dist_temp.ndim != 2 or dist_temp.shape[1] != 1:
+        raise TypeError("Dist_temp must be a column vector.")
 
-    if not isinstance(temp_mod, np.ndarray): 
-        raise TypeError("Temp_mod must be a numpy array.")
-    if not isinstance(temp, np.ndarray):
-        raise TypeError("Temp must be a numpy array.")
+    #Isinstance - https://docs.python.org/3/library/functions.html#isinstance
+    if not isinstance(temp_mod, np.array): 
+        raise TypeError("Temp_mod must be a numpy array representing a matrix.")
+    if not isinstance(temp, np.array):
+        raise TypeError("Temp must be a numpy array representing a matrix.")
     
     if unattend:
         print('...Done!')
@@ -59,14 +65,14 @@ def handle_errors(*args):
         print('Resampling data to original spatial and temporal resolution...')
 
     result_list = []
-    for i in range(time_mod.shape[1]):
-        result = interpolation_func(dist_mod[0, :], temp_mod[:, i], dist_temp[0, :], 'linear')
-        result_list.append(result)
+    for i in range(len(time_mod)):
+       result = interpolation_func(dist_mod[:,0], temp_mod[:,i], dist_temp[:,0])
+       result_list.append(result)
     temp_dx = np.array(result_list)
 
     result_list = []
-    for i in range(dist_temp.shape[1]):
-        result = interpolation_func(time_mod[0, :], temp_dx[i, :], time_temp[0, :], 'linear')
+    for i in range(len(dist_temp)):
+        result = interpolation_func(time_mod[:,0], temp_dx[i,:], time_temp[:,0])
         result_list.append(result)
     temp_dt = np.array(result_list)
 
@@ -122,4 +128,7 @@ def handle_errors(*args):
         plt.show()
 
         return rel_err, me, mae, mse, rmse, nrmse 
+
+                 
+
 
