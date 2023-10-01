216f31691610a4c315745ad4225dfe4a060bc51b
dustykeyboard1 <mikesyanks02@icloud.com>
2023-10-01 11:59:42 -0400

    For integration

--- Python/Scripts/script_to_run.py
+++ Python/Scripts/script_to_run.py
@@ -36,4 +36,21 @@ unattend = True
 
 # Call the handle_errors function
 rel_err, me, mae, mse, rmse, nrmse = handle_errors(time_mod, dist_mod, temp_mod, dist_temp, time_temp, temp, unattend)
-sens = sens(input_data, )
+sens = sens(input_data, [-0.01, 0.01],[-2, 2],[-0.1, 0.1],[-0.1, 0.1])
+
+# Save output to CSV files
+# np.savetxt() - https://numpy.org/doc/stable/reference/generated/numpy.savetxt.html
+path = "Python/Results/CSVs/"
+np.savetxt(f"{path}temp_mod.csv", temp_mod, delimiter=",")
+np.savetxt(f"{path}temp.csv", temp, delimiter=",")
+np.savetxt(f"{path}rel_err.csv", rel_err, delimiter=",")
+np.savetxt(f"{path}heatflux_data.csv", flux_data['heatflux'], delimiter=",")
+np.savetxt(f"{path}solarflux_data.csv", flux_data['solarflux'], delimiter=",")
+np.savetxt(f"{path}solar_refl_data.csv", flux_data['solar_refl'], delimiter=",")
+np.savetxt(f"{path}long_data.csv", flux_data['long'], delimiter=",")
+np.savetxt(f"{path}atmflux_data.csv", flux_data['atmflux'], delimiter=",")
+np.savetxt(f"{path}landflux_data.csv", flux_data['landflux'], delimiter=",")
+np.savetxt(f"{path}backrad_data.csv", flux_data['backrad'], delimiter=",")
+np.savetxt(f"{path}evap_data.csv", flux_data['evap'], delimiter=",")
+np.savetxt(f"{path}sensible_data.csv", flux_data['sensible'], delimiter=",")
+np.savetxt(f"{path}conduction_data.csv", flux_data['conduction'], delimiter=",")
