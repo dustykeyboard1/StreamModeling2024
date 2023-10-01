8a16b6a1c785384642177400b3c95e43d77fdfef
VioletShi-_- <passionatefrisbee@gmail.com>
2023-10-01 16:21:57 -0400

    heat map done yay, need to fix 3D

--- Python/src/Core/hflux.py
+++ Python/src/Core/hflux.py
@@ -3,6 +3,10 @@ import os
 import sys
 import math
 from datetime import datetime
+from scipy.sparse import csc_matrix
+from scipy.sparse.linalg import spsolve
+import matplotlib.pyplot as plt
+import pandas as pd
 
 #Find the root directory dynmimically. https://stackoverflow.com/questions/73230007/how-can-i-set-a-root-directory-dynamically
 root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
@@ -232,7 +236,7 @@ def hflux():
     q_l_min = q_l * 60
 
     print('...done!')
-    method = 2
+    method = 1
     match method:
         case 1:
             ###############################################################
@@ -296,13 +300,7 @@ def hflux():
                                                     wind_speed_mat[:, 0], z, sed, bed_temp_dt[:, 0],
                                                     depth_of_meas_m, shade_m, vts_m,
                                                     cl[:, 0], sol_refl[0], wp_m[:r, 0], width_m[:, 0])
-            
-
-            # print(sed)
-            # for x in heat_flux[:, 0]:
-            #     print(x)
-                #   , shortwave[:11, 0], longwave[:11, 0], atm[:11, 0], back[:11, 0], land[:11, 0], latent[:11, 0], sensible[:11, 0], bed[:11, 0])
-            
+             
             rho_water = 1000
             c_water = 4182
 
@@ -320,10 +318,9 @@ def hflux():
                     else:
                         k[i, j] = (dt * q_l_min[i,j]) / (volume[i, 0])
 
-            m = np.zeros(width_m.shape)
-            d = np.zeros(width_m.shape)
 
-            ### THIS TAKES 8 MINUTES
+            m = np.zeros(width_m.shape)
+            d = np.zeros(width_m.shape)     
             for i in range(timesteps - 1):
                 m[:,i] = (dt * (width_m[:,i] * heat_flux[:,i]) / ((rho_water * c_water)) / area_m[:,i])
                 d[0,i] = ((g[0,i] * t[0,i]) + (o_c[0,i] * temp_x0_dt[i]) - (p_c[0,i] * t[1,i]) + (k[0,i] * t_l_m[0]) + m[0,i]) - (a_c[0,i] * temp_x0_dt[i+1])
@@ -337,7 +334,8 @@ def hflux():
                     loop_array[j, j + 1] = c[j, i]
                     loop_array[r - 1, r - 1] = b[r - 1, i] + c[r - 1, i]
 
-                t[:, i + 1] = np.linalg.solve(loop_array, d[:,i])
+                loop_array = csc_matrix(loop_array)
+                t[:, i + 1] = spsolve(loop_array, d[:,i])
 
                 heat_flux[:,i+1], shortwave[:,i+1], longwave[:,i+1],atm[:,i+1], back[:,i+1], land[:,i+1], latent[:,i+1],sensible[:,i+1], bed[:,i+1] = hflux_flux(input_data["settings"],solar_rad_mat[:,i+1],air_temp_mat[:,i+1],
                     rel_hum_mat[:,i+1],t[:,i+1],wind_speed_mat[:,i+1],z,
@@ -345,109 +343,230 @@ def hflux():
                     shade_m,vts_m,cl[:,i+1],sol_refl[i+1],wp_m[:,i+1], width_m[:,i+1])
 
             ### It APPEARS that BOTH t, d are correct, I will rigorously verify this tomorrow
-            print(t)
-            print(d)
-        case 2:
-            t = np.zeros((r, timesteps))
-            t[:,0] = temp_t0_m
-            t[0,:] = temp_x0_dt
-            t_k1 = np.zeros((r, timesteps))
-            heat_flux = np.zeros((r, timesteps))
+            # print("here")
+            # print(m.shape)
+            # print(t)
+            # print(d)
+        # case 2:
+        #     t = np.zeros((r, timesteps))
+        #     t[:,0] = temp_t0_m
+        #     t[0,:] = temp_x0_dt
+        #     t_k1 = np.zeros((r, timesteps))
+        #     heat_flux = np.zeros((r, timesteps))
 
-            heat_flux = np.empty((r, timesteps))
-            heat_flux_k1 = np.empty((r, timesteps))
-            heat_flux_k2 = np.empty((r, timesteps))
-            shortwave = np.empty((r, timesteps))
-            longwave = np.empty((r, timesteps))
-            atm = np.empty((r, timesteps))
-            back = np.empty((r, timesteps))
-            land = np.empty((r, timesteps))
-            latent = np.empty((r, timesteps))
-            sensible = np.empty((r, timesteps))
-            bed = np.empty((r, timesteps))
-            u1 = np.empty((r, timesteps))
-            v1 = np.empty((r, timesteps))
-            s1 = np.empty((r, timesteps))
-            m1 = np.empty((r, timesteps))
-            k1 = np.empty((r, timesteps))
-            u2 = np.empty((r, timesteps))
-            v2 = np.empty((r, timesteps))
-            s2 = np.empty((r, timesteps))
-            m2 = np.empty((r, timesteps))
-            k2 = np.empty((r, timesteps))
+        #     heat_flux = np.empty((r, timesteps))
+        #     heat_flux_k1 = np.empty((r, timesteps))
+        #     heat_flux_k2 = np.empty((r, timesteps))
+        #     shortwave = np.empty((r, timesteps))
+        #     longwave = np.empty((r, timesteps))
+        #     atm = np.empty((r, timesteps))
+        #     back = np.empty((r, timesteps))
+        #     land = np.empty((r, timesteps))
+        #     latent = np.empty((r, timesteps))
+        #     sensible = np.empty((r, timesteps))
+        #     bed = np.empty((r, timesteps))
+        #     u1 = np.empty((r, timesteps))
+        #     v1 = np.empty((r, timesteps))
+        #     s1 = np.empty((r, timesteps))
+        #     m1 = np.empty((r, timesteps))
+        #     k1 = np.empty((r, timesteps))
+        #     u2 = np.empty((r, timesteps))
+        #     v2 = np.empty((r, timesteps))
+        #     s2 = np.empty((r, timesteps))
+        #     m2 = np.empty((r, timesteps))
+        #     k2 = np.empty((r, timesteps))
 
-            heat_flux[:, 0], shortwave[:, 0], longwave[:, 0], atm[:, 0], back[:, 0], land[:, 0], latent[:, 0], sensible[:, 0], bed[:, 0] = hflux_flux(input_data["settings"], solar_rad_mat[:, 0],
-                                                    air_temp_mat[:, 0], rel_hum_mat[:, 0], temp_t0_m,
-                                                    wind_speed_mat[:, 0], z, sed, bed_temp_dt[:, 0],
-                                                    depth_of_meas_m, shade_m, vts_m,
-                                                    cl[:, 0], sol_refl[0], wp_m[:r, 0], width_m[:, 0])
+        #     heat_flux[:, 0], shortwave[:, 0], longwave[:, 0], atm[:, 0], back[:, 0], land[:, 0], latent[:, 0], sensible[:, 0], bed[:, 0] = hflux_flux(input_data["settings"], solar_rad_mat[:, 0],
+        #                                             air_temp_mat[:, 0], rel_hum_mat[:, 0], temp_t0_m,
+        #                                             wind_speed_mat[:, 0], z, sed, bed_temp_dt[:, 0],
+        #                                             depth_of_meas_m, shade_m, vts_m,
+        #                                             cl[:, 0], sol_refl[0], wp_m[:r, 0], width_m[:, 0])
 
-            for i in range(timesteps - 1):
-                heat_flux_k1[:, i], _, _, _, _, _, _, _, _ = hflux_flux(input_data["settings"], solar_rad_mat[:, i],
-                                                    air_temp_mat[:, i], rel_hum_mat[:, i], t[:, i],
-                                                    wind_speed_mat[:, i], z, sed, bed_temp_dt[:, i],
-                                                    depth_of_meas_m, shade_m, vts_m,
-                                                    cl[:, i], sol_refl[i], wp_m[:, i], width_m[:, i])
+        #     for i in range(timesteps - 1):
+        #         heat_flux_k1[:, i], _, _, _, _, _, _, _, _ = hflux_flux(input_data["settings"], solar_rad_mat[:, i],
+        #                                             air_temp_mat[:, i], rel_hum_mat[:, i], t[:, i],
+        #                                             wind_speed_mat[:, i], z, sed, bed_temp_dt[:, i],
+        #                                             depth_of_meas_m, shade_m, vts_m,
+        #                                             cl[:, i], sol_refl[i], wp_m[:, i], width_m[:, i])
 
-                for j in range(1, r - 1):
-                    u1[j, i] = (q_half_min[j,i] / volume[j, 0]) * (.5 * t[j - 1, i] - .5 * t[j, i])
-                    v1[j, i]=(q_half_min[j + 1, i] / volume[j, 0]) * (0.5 * t[j, i] - 0.5 * t[j + 1,i])
-                    s1[j, i]=(q_l_min[j, i] / volume[j, 0]) * (t_l_m[j] - t[j, i])
+        #         for j in range(1, r - 1):
+        #             u1[j, i] = (q_half_min[j,i] / volume[j, 0]) * (.5 * t[j - 1, i] - .5 * t[j, i])
+        #             v1[j, i]=(q_half_min[j + 1, i] / volume[j, 0]) * (0.5 * t[j, i] - 0.5 * t[j + 1,i])
+        #             s1[j, i]=(q_l_min[j, i] / volume[j, 0]) * (t_l_m[j] - t[j, i])
 
-                    rho_water = 1000
-                    c_water = 4182
-                    m1[j, i] = (width_m[j,i] * heat_flux_k1[j,i]) / ((rho_water*c_water)) / area_m[j, i]
-                    k1[j, i] = u1[j, i] + v1[j, i] + s1[j, i] + m1[j, i]
+        #             rho_water = 1000
+        #             c_water = 4182
+        #             m1[j, i] = (width_m[j,i] * heat_flux_k1[j,i]) / ((rho_water*c_water)) / area_m[j, i]
+        #             k1[j, i] = u1[j, i] + v1[j, i] + s1[j, i] + m1[j, i]
                 
-                u1[r - 1, i] = (q_half_min[r - 1,i] / volume[r - 1, 0]) * (.5 * t[r - 2, i] - .5 * t[r - 1, i])
-                v1[r - 1, i]=(q_half_min[r, i] / volume[r - 1, 0]) * (0.5 * t[r - 1, i] - 0.5 * t[r - 1,i])
-                s1[r - 1, i]=(q_l_min[r - 1, i] / volume[r - 1, 0]) * (t_l_m[r - 1] - t[r - 1, i])
+        #         u1[r - 1, i] = (q_half_min[r - 1,i] / volume[r - 1, 0]) * (.5 * t[r - 2, i] - .5 * t[r - 1, i])
+        #         v1[r - 1, i]=(q_half_min[r, i] / volume[r - 1, 0]) * (0.5 * t[r - 1, i] - 0.5 * t[r - 1,i])
+        #         s1[r - 1, i]=(q_l_min[r - 1, i] / volume[r - 1, 0]) * (t_l_m[r - 1] - t[r - 1, i])
 
-                m1[r - 1, i] = (width_m[r - 1,i] * heat_flux_k1[r - 1,i]) / ((rho_water*c_water)) / area_m[r - 1, i]
-                k1[r - 1, i] = u1[r - 1, i] + v1[r - 1, i] + s1[r - 1, i] + m1[r - 1, i]
+        #         m1[r - 1, i] = (width_m[r - 1,i] * heat_flux_k1[r - 1,i]) / ((rho_water*c_water)) / area_m[r - 1, i]
+        #         k1[r - 1, i] = u1[r - 1, i] + v1[r - 1, i] + s1[r - 1, i] + m1[r - 1, i]
 
-                # Calculate temp based on k1
-                t_k1[0, i] = temp_x0_dt[i]
-                for j in range(1, r):
-                    t_k1[j, i] = t[j, i] + (dt * k1[j, i])
+        #         # Calculate temp based on k1
+        #         t_k1[0, i] = temp_x0_dt[i]
+        #         for j in range(1, r):
+        #             t_k1[j, i] = t[j, i] + (dt * k1[j, i])
                 
-                heat_flux_k2[:, i], _, _, _, _, _, _, _, _ = hflux_flux(input_data["settings"], solar_rad_mat[:, i],
-                                                    air_temp_mat[:, i], rel_hum_mat[:, i], t_k1[:, i],
-                                                    wind_speed_mat[:, i], z, sed, bed_temp_dt[:, i],
-                                                    depth_of_meas_m, shade_m, vts_m,
-                                                    cl[:, i], sol_refl[i], wp_m[:, i], width_m[:, i])
+        #         heat_flux_k2[:, i], _, _, _, _, _, _, _, _ = hflux_flux(input_data["settings"], solar_rad_mat[:, i],
+        #                                             air_temp_mat[:, i], rel_hum_mat[:, i], t_k1[:, i],
+        #                                             wind_speed_mat[:, i], z, sed, bed_temp_dt[:, i],
+        #                                             depth_of_meas_m, shade_m, vts_m,
+        #                                             cl[:, i], sol_refl[i], wp_m[:, i], width_m[:, i])
 
-                for j in range(1, r - 1):
-                    u2[j, i] = (q_half_min[j, i + 1] / volume[j, 0]) * (.5 * t_k1[j - 1, i] - .5 * t_k1[j, i])
-                    v2[j, i]=(q_half_min[j + 1, i + 1] / volume[j, 0]) * (0.5 * t_k1[j, i] - 0.5 * t_k1[j + 1,i])
-                    s2[j, i]=(q_l_min[j, i + 1] / volume[j, 0]) * (t_l_m[j] - t_k1[j, i])
+        #         for j in range(1, r - 1):
+        #             u2[j, i] = (q_half_min[j, i + 1] / volume[j, 0]) * (.5 * t_k1[j - 1, i] - .5 * t_k1[j, i])
+        #             v2[j, i]=(q_half_min[j + 1, i + 1] / volume[j, 0]) * (0.5 * t_k1[j, i] - 0.5 * t_k1[j + 1,i])
+        #             s2[j, i]=(q_l_min[j, i + 1] / volume[j, 0]) * (t_l_m[j] - t_k1[j, i])
 
-                    m2[j, i] = (width_m[j,i] * heat_flux_k2[j,i]) / ((rho_water*c_water)) / area_m[j, i]
-                    k2[j, i] = u2[j, i] + v2[j, i] + s2[j, i] + m2[j, i]
+        #             m2[j, i] = (width_m[j,i] * heat_flux_k2[j,i]) / ((rho_water*c_water)) / area_m[j, i]
+        #             k2[j, i] = u2[j, i] + v2[j, i] + s2[j, i] + m2[j, i]
                 
-                u2[r - 1, i] = (q_half_min[r - 1, i + 1] / volume[r - 1, 0]) * (.5 * t_k1[r - 2, i] - .5 * t_k1[r - 1, i])
-                v2[r - 1, i]=(q_half_min[r, i] / volume[r - 1, 0]) * (0.5 * t_k1[r - 1, i] - 0.5 * t_k1[r - 1,i])
-                s2[r - 1, i]=(q_l_min[r - 1, i] / volume[r - 1, 0]) * (t_l_m[r - 1] - t_k1[r - 1, i])
+        #         u2[r - 1, i] = (q_half_min[r - 1, i + 1] / volume[r - 1, 0]) * (.5 * t_k1[r - 2, i] - .5 * t_k1[r - 1, i])
+        #         v2[r - 1, i]=(q_half_min[r, i] / volume[r - 1, 0]) * (0.5 * t_k1[r - 1, i] - 0.5 * t_k1[r - 1,i])
+        #         s2[r - 1, i]=(q_l_min[r - 1, i] / volume[r - 1, 0]) * (t_l_m[r - 1] - t_k1[r - 1, i])
 
-                m2[r - 1, i] = (width_m[r - 1,i] * heat_flux_k2[r - 1,i]) / ((rho_water*c_water)) / area_m[r - 1, i]
-                k2[r - 1, i] = u2[r - 1, i] + v2[r - 1, i] + s2[r - 1, i] + m2[r - 1, i]
+        #         m2[r - 1, i] = (width_m[r - 1,i] * heat_flux_k2[r - 1,i]) / ((rho_water*c_water)) / area_m[r - 1, i]
+        #         k2[r - 1, i] = u2[r - 1, i] + v2[r - 1, i] + s2[r - 1, i] + m2[r - 1, i]
                 
-                for j in range(1, r):
-                    t[j, i + 1] = t[j, i] + (dt * (.5 * k1[j, i]) + .5 * k2[j, i])
+        #         for j in range(1, r):
+        #             t[j, i + 1] = t[j, i] + (dt * (.5 * k1[j, i]) + .5 * k2[j, i])
 
-                heat_flux[:,i+1], shortwave[:,i+1], longwave[:,i + 1], atm[:,i + 1], back[:,i + 1], land[:,i + 1], latent[:,i + 1],sensible[:,i + 1], bed[:,i + 1] = hflux_flux(input_data["settings"],
-                    solar_rad_mat[:,i+1],air_temp_mat[:,i+1],
-                    rel_hum_mat[:,i+1],t[:,i+1],wind_speed_mat[:,i+1],z,
-                    sed,bed_temp_dt[:,i+1],depth_of_meas_m,
-                    shade_m,vts_m,cl[:,i+1],sol_refl[i + 1], wp_m[:,i+1], width_m[:,i+1])
-                print(i)
+        #         heat_flux[:,i+1], shortwave[:,i+1], longwave[:,i + 1], atm[:,i + 1], back[:,i + 1], land[:,i + 1], latent[:,i + 1],sensible[:,i + 1], bed[:,i + 1] = hflux_flux(input_data["settings"],
+        #             solar_rad_mat[:,i+1],air_temp_mat[:,i+1],
+        #             rel_hum_mat[:,i+1],t[:,i+1],wind_speed_mat[:,i+1],z,
+        #             sed,bed_temp_dt[:,i+1],depth_of_meas_m,
+        #             shade_m,vts_m,cl[:,i+1],sol_refl[i + 1], wp_m[:,i+1], width_m[:,i+1])
+        #         print(i)
                 
-            print(heat_flux)
-                
-### Make Plots
-                
+        #     print(heat_flux)
 
+    #2D Plot of stream temperature
 
+    # Create a figure
+    fig, ax = plt.subplots()
+
+    # ax.imshow() - https://matplotlib.org/stable/api/_as_gen/matplotlib.axes.Axes.imshow.html
+    cax = ax.imshow(t, aspect='auto', cmap='jet', origin='lower',
+                    extent=[np.min(time_mod), np.max(time_mod), np.min(dist_mod), np.max(dist_mod)])
+
+    # Add a colorbar with label
+    # plt.colorbar() - https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.colorbar.html
+    cbar = plt.colorbar(cax)
+    cbar.set_label('Temperature (°C)', fontsize=12, fontweight='bold')
+
+    # Set title and labels
+    plot_title = 'Modeled Stream Temperature'
+    xlab = 'Time (min)'
+    ylab = 'Distance (m)'
+    ax.set_title(plot_title, fontsize=12, fontweight='bold')
+    ax.set_xlabel(xlab, fontsize=11)
+    ax.set_ylabel(ylab, fontsize=11)
+
+    # ax.invert_yaxis() - https://www.geeksforgeeks.org/how-to-reverse-axes-in-matplotlib/
+    ax.invert_yaxis()
+
+    plt.show()
+
+    #3D plot of stream temperature
+    fig = plt.figure()
+
+    # Create a 3D axis
+    ax = fig.add_subplot(111, projection='3d')
+
+    # Create a surface plot
+    # ax.plot_surface() - https://matplotlib.org/stable/plot_types/3D/surface3d_simple.html#plot-surface-x-y-z
+    surface = ax.plot_surface(time_mod, dist_mod, t, cmap='jet')
+
+    # Add a colorbar with label
+    cbar = fig.colorbar(surface)
+    cbar.set_label('Temperature (°C)', fontsize=11, fontweight='bold')
+
+    # Set title and labels
+    plot_title = 'Modeled Stream Temperature'
+    xlab = 'Time (min)'
+    ylab = 'Distance (m)'
+    zlab = 'Temp (°C)'
+    ax.set_title(plot_title, fontsize=12, fontweight='bold')
+    ax.set_xlabel(xlab, fontsize=11)
+    ax.set_ylabel(ylab, fontsize=11)
+    ax.set_zlabel(zlab, fontsize=11)
+
+    plt.show(block = False)
+
+    #Plot of heat fluxes
+    fig = plt.figure()
+
+    # Subplot 1
+    plt.subplot(3, 2, 1)
+    plt.plot(time_mod, np.mean(heat_flux / 60, axis=0), 'k')
+    plt.title('Total Heat Flux', fontweight='bold')
+    plt.ylabel('Energy Flux (W/m^2)', fontweight='bold')
+    plt.axis([np.min(time_mod), np.max(time_mod), np.min(np.mean(heat_flux / 60, axis=0)), np.max(np.mean(heat_flux / 60, axis=0))])
+
+    # Subplot 2
+    plt.subplot(3, 2, 2)
+    plt.plot(time_mod, np.mean(shortwave, axis=0), 'r')
+    plt.title('Shortwave Radiation', fontweight='bold')
+    plt.axis([np.min(time_mod), np.max(time_mod), np.min(np.mean(shortwave, axis=0)), np.max(np.mean(shortwave, axis=0))])
+
+    # Subplot 3
+    plt.subplot(3, 2, 3)
+    plt.plot(time_mod, np.mean(longwave, axis=0), 'b')
+    plt.title('Longwave Radiation', fontweight='bold')
+    plt.ylabel('Energy Flux (W/m^2)', fontweight='bold')
+    plt.axis([np.min(time_mod), np.max(time_mod), np.min(np.mean(longwave, axis=0)), np.max(np.mean(longwave, axis=0))])
+
+    # Subplot 4
+    plt.subplot(3, 2, 4)
+    plt.plot(time_mod, np.mean(latent, axis=0), 'g')
+    plt.title('Latent Heat Flux', fontweight='bold')
+    plt.axis([np.min(time_mod), np.max(time_mod), np.min(np.mean(latent, axis=0)), np.max(np.mean(latent, axis=0))])
+
+    # Subplot 5
+    plt.subplot(3, 2, 5)
+    plt.plot(time_mod, np.mean(bed, axis=0), 'm')
+    plt.title('Bed Conduction', fontweight='bold')
+    plt.xlabel('Time (min)', fontweight='bold')
+    plt.ylabel('Energy Flux (W/m^2)', fontweight='bold')
+    plt.axis([np.min(time_mod), np.max(time_mod), np.min(np.mean(bed, axis=0)), np.max(np.mean(bed, axis=0))])
+
+    # Subplot 6
+    plt.subplot(3, 2, 6)
+    plt.plot(time_mod, np.mean(sensible, axis=0), 'y')
+    plt.title('Sensible Heat Flux', fontweight='bold')
+    plt.xlabel('Time (min)', fontweight='bold')
+    plt.axis([np.min(time_mod), np.max(time_mod), np.min(np.mean(sensible, axis=0)), np.max(np.mean(sensible, axis=0))])
+
+    plt.show(block = False)
+
+    #Plot of heat fluxes: comparison 
+    plt.figure()
+
+    # Plot data
+    plt.plot(time_mod, np.mean(heat_flux / 60, axis=0), 'k', label='Total Heat Flux')
+    plt.plot(time_mod, np.mean(shortwave, axis=0), 'r', label='Solar Radiation')
+    plt.plot(time_mod, np.mean(longwave, axis=0), 'b', label='Longwave Radiation')
+    plt.plot(time_mod, np.mean(latent, axis=0), 'g', label='Latent Heat Flux')
+    plt.plot(time_mod, np.mean(bed, axis=0), 'c', label='Streambed Conduction')
+    plt.plot(time_mod, np.mean(sensible, axis=0), 'm', label='Sensible Heat Flux')
+
+    # Set axis properties
+    plt.axis('normal')
+    plt.xlim([np.min(time_mod), np.max(time_mod)])
+
+    # Add title and labels
+    plt.title('Energy Fluxes', fontsize=12, fontweight='bold')
+    plt.xlabel('Time (min)', fontsize=11, fontweight='bold')
+    plt.ylabel('Energy Flux (W/m^2)', fontsize=11, fontweight='bold')
+
+    # Add legend
+    plt.legend(loc='best')
+
+    plt.show(block = False)
 
 
 hflux()
